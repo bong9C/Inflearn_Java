882e6f9b49c2e778312e85ba106d203aa9de3127
bong9C <kathy1221@naver.com>
2023-12-10 18:42:21 +0900

    23.12.10 인프런 D-1

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hellojava1"); //hello java1을 출력합니다.
+        //System.out.println("hellojava2");
+
+        /*
+        System.out.println("hellojava3");
+        System.out.println("hellojava4");
+         */
+
+    }
+}
