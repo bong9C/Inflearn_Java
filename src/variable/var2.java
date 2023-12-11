1c819579f11221f9f625989cad4c204a722a337b
bong9C <kathy1221@naver.com>
2023-12-11 14:51:26 +0900

    23.12.10 인프런 D-2

---
+++ src/variable/var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class var2 {
+    public static void main(String[] args) {
+        int a; // 변수 선언 - 정수 숫자를 담을 수 있는 그릇
+        a = 20; // 10 -> 20
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
