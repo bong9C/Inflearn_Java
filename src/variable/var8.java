1c6cb15479a652db8c62710829cb7f782bdd809c
bong9C <kathy1221@naver.com>
2023-12-12 19:47:37 +0900

    23.12.10 인프런 D-3

---
+++ src/variable/var8.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // - 21억 ~ 21억 (약 20억)
+
+
+        // 실수
+        float f = 10.0f;
+        double d= 10.0;  // 더블이 플롯보다 더 큰 실수를 사용할 수 있다.
+    }
+}
