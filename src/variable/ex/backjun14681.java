c1b02c24a4420aa70b4f2fa5344ce81af56abce5
bong9C <kathy1221@naver.com>
2023-12-16 18:49:31 +0900

    23.12.10 백준문제풀이 D-1

--- src/variable/ex/backjun14681.java
+++ src/variable/ex/backjun14681.java
@@ -1,31 +1,34 @@
 package variable.ex;
 import java.util.Scanner;
+
 public class backjun14681 {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Scanner 객체 생성
+        Scanner in = new Scanner(System.in);
 
-Scanner in = new Scanner(System.in);
+        // 사용자로부터 X와 Y 좌표를 입력받음
+        int X = in.nextInt();
+        int Y = in.nextInt();
 
-int X = in.nextInt();
-int Y = in.nextInt();
+        // X가 양수인 경우
+        if (X > 0) {
+            // Y가 양수인 경우
+            if (Y > 0) {
+                System.out.println(1); // 1사분면에 속함
+            } else {
+                System.out.println(4); // 4사분면에 속함
+            }
+        } else {
+            // X가 음수인 경우
+            if (Y > 0) {
+                System.out.println(2); // 2사분면에 속함
+            } else {
+                System.out.println(3); // 3사분면에 속함
+            }
+        }
 
-if(X > 0) {
-if(Y > 0) {
-    System.out.println(1);
-}
-else {
-    System.out.println(4);
-}
-}
-
-else {
-if(Y > 0) {
-    System.out.println(2);
-}
-else {
-    System.out.println(3);
-}
-}
-}
-
+        // Scanner 객체 닫기 (자원 해제)
+        in.close();
+    }
 }
