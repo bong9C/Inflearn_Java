4acce7e98436d5772c88cae6aef2acfb126632ce
bong9C <kathy1221@naver.com>
2023-12-29 22:58:22 +0900

    23.12.29 인프런 D-4

---
+++ src/operator/Opertor1.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class Opertor1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+
+        // 뺼셈
+        int diff = a - b;
+        System.out.println("a - b = " + diff);
+
+        // 곱셈
+        int multi = a * b;
+        System.out.println("a * b = " + multi);
+
+        // 나눗셈
+        int div = a / b;  // 원래 답은 2.5인데 int 로 계산해서 int 형 답이 나옴.
+        System.out.println("a / b = " + div);
+
+        // 나머지
+        int mod = a % b;
+        System.out.println("a % b = " + mod);
+    }
+}
